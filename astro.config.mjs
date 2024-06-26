7ddebd38d85eb65566d24ce6611ed5967a5ad56a
thirking <979339510@qq.com>
2024-06-27 01:14:09 +0800

    add discu/ modify header style

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://wwww.thirking.top',
   integrations: [mdx(), sitemap(), svelte(), tailwind()],
   output: "server",
   adapter: node({
